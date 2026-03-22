b8f77d5b09a0c6edf1c74c2dce818f5dd179badb
Oliver Ernster <oernster@users.noreply.github.com>
2026-03-22 20:27:53 +0000

    Remove public epub pipeline gen surface.

--- book/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Book-building utilities.
-
-This directory also contains the book assets (cover, CSS, metadata, etc.).
-"""
--- book/book_builder/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Decision Architecture book builder package."""
-
-from book.book_builder.orchestrator import BuildOrchestrator
-
-__all__ = ["BuildOrchestrator"]
